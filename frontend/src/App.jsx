dcc4c288f2a56267aec9c70fe8f762cc78cd005d
Roshan Raj Sinha <roshan1395sinha@gmail.com>
2025-05-25 00:41:42 +0530

    Add initial frontend structure with assets, pages, and components
    - Added images for Stripe logo, upload area, upload icon, and verified icon.
    - Created Navbar component.
    - Set up main entry point with routing.
    - Added About, Appointment, Contact, Doctors, Home, Login, MyAppointments, and MyProfile pages.
    - Configured Tailwind CSS in index.css and Vite for styling.

---
+++ frontend/src/App.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import Doctors from './pages/Doctors'
+import Contact from './pages/Contact'
+import MyAppointments from './pages/MyAppointments'
+import Login from './pages/Login'
+import MyProfile from './pages/MyProfile'
+import Appointment from './pages/Appointment'
+import Navbar from './components/Navbar'
+
+const App = () => {
+  return (
+    <div className='mx-4 sm:mx-[10%]'>
+        <Navbar/>
+        <Routes>
+          <Route path='/' element={<Home/>}/>
+          <Route path='/about' element={<About/>}/>
+          <Route path='/doctors/:speciality' element={<Doctors/>}/>
+          <Route path='/contact' element={<Contact/>}/>
+          <Route path='/Login' element={<Login/>}/>
+          <Route path='/My-Appointment' element={<MyAppointments/>}/>
+          <Route path='/My-Profile' element={<MyProfile/>}/>
+          <Route path='/appointment' element={<Appointment/>}/>
+        </Routes>
+    </div>
+  )
+}
+
+export default App
---
+++ frontend/src/pages/Login.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Login = () => {
+  return (
+    <div className='text-green-500'>
+      hello
+    </div>
+  )
+}
+
+export default Login
